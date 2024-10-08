7058bd23c4d35bc4b02ef4490e22c08b1e064d76
Woojin1123 <boom175839@gmail.com>
2024-10-08 19:59:58 +0900

    [FIX] TestCode security인증 문제 수정

--- src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
+++ src/test/java/org/example/expert/domain/todo/controller/TodoControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TodoController.class)
+@WithMockUser(username = "testUser", roles = {"USER"})
 class TodoControllerTest {
 
     @Autowired
